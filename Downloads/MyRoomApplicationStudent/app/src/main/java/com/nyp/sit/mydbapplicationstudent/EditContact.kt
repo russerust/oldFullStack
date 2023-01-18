13f8d6285a6a880a5a03ecbbac0edc9537851c4e
russerust <dabsdubs@gmail.com>
2023-01-18 23:06:17 +0800

    test

---
+++ Downloads/MyRoomApplicationStudent/app/src/main/java/com/nyp/sit/mydbapplicationstudent/EditContact.kt
@@ -0,0 +1,11 @@
+package com.nyp.sit.mydbapplicationstudent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditContact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_contact)
+    }
+}
